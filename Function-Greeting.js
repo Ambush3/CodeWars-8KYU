668f0624e9a5126b03185173009432fadc7abdfe
aaronbush3 <aaronbush3@gmail.com>
2022-04-18 06:38:13 -0400

    Function that returns a greeting to inputted name

---
+++ Function-Greeting.js
@@ -0,0 +1,6 @@
+// create a function that returns a string that says "Hello, <name> how are you doing today?"
+
+function greet(name) {
+    //your code here
+    return `Hello, ${name} how are you doing today?`
+}
