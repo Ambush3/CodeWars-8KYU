4a9831973f2214645f43f1e68d78be290508c2ac
aaronbush3 <aaronbush3@gmail.com>
2022-05-01 22:50:33 -0400

    If given number is even, multiply number by 8. If number is odd, multiply number by 9

---
+++ Simple-Multiplication.js
@@ -0,0 +1,11 @@
+// if given number is even, return number * 8
+// if given number is odd, return number * 9
+
+function simpleMultiplication(number) {
+    // your code........
+    if (number % 2 === 0) {
+        return number * 8
+    } else {
+        return number * 9
+    }
+}
