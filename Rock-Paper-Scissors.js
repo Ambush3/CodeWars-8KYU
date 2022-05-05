26d76858055c88b999385424a169d90e4a33bcd8
aaronbush3 <aaronbush3@gmail.com>
2022-05-05 18:53:29 -0400

    Create rock paper scissors

---
+++ Rock-Paper-Scissors.js
@@ -0,0 +1,24 @@
+const rps = (p1, p2) => {
+    if (p1 === p2) {
+        return 'Draw!';
+    }
+    if (p1 === 'rock') {
+        if (p2 === 'scissors') {
+            return 'Player 1 won!';
+        }
+        return 'Player 2 won!';
+    }
+    if (p1 === 'paper') {
+        if (p2 === 'rock') {
+            return 'Player 1 won!';
+        }
+        return 'Player 2 won!';
+    }
+    if (p1 === 'scissors') {
+        if (p2 === 'paper') {
+            return 'Player 1 won!';
+        }
+        return 'Player 2 won!';
+    }
+
+};
