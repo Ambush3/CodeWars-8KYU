cbacedd6a57b01fa6be39600385908fab508c87d
aaronbush3 <aaronbush3@gmail.com>
2022-06-04 09:59:00 -0400

    If the animal is an alligator, return small. If animal is anything but an alligator, return wide

---
+++ The-Wide-Mouthed-Frog.js
@@ -0,0 +1,9 @@
+// if animal is alligator, return 'small' else return 'wide'
+
+function mouthSize(animal) {
+    if (animal.toLowerCase() == 'alligator') {
+        return 'small'
+    } else {
+        return 'wide'
+    }
+}
