dfed9c10379d5011fa048a4409e33a1aa45e90b0
aaronbush3 <aaronbush3@gmail.com>
2022-06-06 05:49:43 -0400

    create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array

---
+++ Enumerable-Magic-25.js
@@ -0,0 +1,4 @@
+// Create a function that accepts a list of array and a number n, and returns a list of array of the first n elements from the list of  array.
+function take(arr, n) {
+    return arr.slice(0, n);
+}
