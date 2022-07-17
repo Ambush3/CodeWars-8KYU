2aa591d34425d3831a03913beb46edfd28c76e33
aaronbush3 <aaronbush3@gmail.com>
2022-07-17 17:33:26 -0400

    Write the function that takes in a Circle object and calculates teh area of that circle.

---
+++ Geometry-Basics-Circle-Area-in-2D.js
@@ -0,0 +1,5 @@
+// Take in a circle object and return the area of the circle
+
+circleArea = (circle) => Math.PI * circle.radius * circle.radius;
+// circle.radius is the radius of the circle
+// math.pi is the pi in the math library
