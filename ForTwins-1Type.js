ea1ad70f5eaf1f598c4a1c7356f4c85f49259dde
aaronbush3 <aaronbush3@gmail.com>
2022-07-16 21:29:33 -0400

    If type of variable and type of type are teh same, return true, else, return false

---
+++ ForTwins-1Type.js
@@ -0,0 +1,6 @@
+// if typeof variable and typeof type are the same, return true
+// else return false
+
+function typeValidation(variable, type) {
+    return typeof variable === type; // return true or false
+}
