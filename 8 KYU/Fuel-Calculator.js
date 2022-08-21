467da607c02be029adf3e44223690cdb4213a9c5
aaronbush3 <aaronbush3@gmail.com>
2022-08-20 21:41:00 -0400

    Return the correct discount for amount of litres purchaseD

---
+++ 8 KYU/Fuel-Calculator.js
@@ -0,0 +1,8 @@
+// Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, up to a maximum discount of 25 cents per litre.But total discount per litre cannot be more than 25 cents.Return the total cost rounded to 2 decimal places.Also you can guess that there will not be negative or non - numeric inputs.
+
+function fuelPrice(litres, pricePerLitre) {
+    for(i = 2; i <= 10; i += 2)
+        if(litres >= i) 
+            pricePerLitre -= .05;
+    return +(litres * pricePerLitre).toFixed(2);
+}
