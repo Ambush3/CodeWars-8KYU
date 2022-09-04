e06c601ad5631843b64c91c8c2e5f84d3e85a59e
aaronbush3 <aaronbush3@gmail.com>
2022-09-03 21:18:48 -0400

    Return true if test and original are anagrams of eachother, false if not

--- 7 KYU/Reverse-Words.js
+++ 7 KYU/Reverse-Words.js
@@ -1,5 +1,3 @@
-// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
-
 function reverseWords(str) {
   return str.split(' ').map(word => word.split('').reverse().join('')).join(' ');
 }
