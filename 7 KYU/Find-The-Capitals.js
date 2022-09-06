3952aba06b985f173a3025d164bae5b9c79b51e6
aaronbush3 <aaronbush3@gmail.com>
2022-09-05 21:43:37 -0400

    Find the capital letters and their indices

--- 7 KYU/Find-The-Capitals.js
+++ 7 KYU/Find-The-Capitals.js
@@ -1,9 +1,9 @@
 // if characters are uppercase in string, return its index
 var capitals = function(word) {
-    let uppercase = [];
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] === word[i].toUpperCase()) {
-            uppercase.push(i);
+    let uppercase = []; // create empty array
+    for (let i = 0; i < word.length; i++) { // loop through string
+        if (word[i] === word[i].toUpperCase()) { // if character is uppercase
+            uppercase.push(i); // push index to array
         }
-    } return uppercase;
+    } return uppercase; // return array
 }
