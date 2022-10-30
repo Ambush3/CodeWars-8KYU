57625d2452bee902780d61776e8266422edf3da7
aaronbush3 <aaronbush3@gmail.com>
2022-10-30 19:56:10 -0400

    Updated comments

--- 7 KYU/Alphabetical-Sequence.js
+++ 7 KYU/Alphabetical-Sequence.js
@@ -1,6 +1,6 @@
 function alphaSeq(str) {
-    let sorted = str.toLowerCase().split('').sort(); // sort the string alphabetically 
-    let repeating = sorted.map(v => v.charCodeAt() - 96); // get the number of times each letter repeats 
-    return sorted.map((v, i) => v.repeat(repeating[i]).slice(0, 1).toUpperCase() + // get the first letter of each sequence and uppercase it 
-        v.repeat(repeating[i]).slice(1)).join(','); // get the rest of the letters and join them with a comma
+    let sorted = str.toLowerCase().split('').sort(); // split string into array, sort array alphabetically and join back into string
+    let repeating = sorted.map(v => v.charCodeAt() - 96); // map each letter to its position in the alphabet 
+    return sorted.map((v, i) => v.repeat(repeating[i]).slice(0, 1).toUpperCase() +  // repeat each letter by its position in the alphabet and capitalize the first letter of each word
+        v.repeat(repeating[i]).slice(1)).join(','); // join the array into a string with commas 
 }
