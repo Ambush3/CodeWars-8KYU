711699ab9907b4ec36e88055bf5d722e113cff60
aaronbush3 <aaronbush3@gmail.com>
2022-10-29 23:11:14 -0400

    Updated with comments

--- 7 KYU/Divide-and-Conquer.js
+++ 7 KYU/Divide-and-Conquer.js
@@ -6,11 +6,11 @@ function divCon(x){
   let num = 0
   let str = 0
   for(let i = 0; i < x.length; i++){
-    if(typeof x[i] === 'string'){
-      str += parseInt(x[i])
+    if(typeof x[i] === 'string'){ // if the element is a string 
+      str += parseInt(x[i]) // add the string to the str variable
     } else {
-      num += x[i]
+      num += x[i] // add the number to the num variable
     }
   }
-  return num - str
+  return num - str // return the difference
 }
