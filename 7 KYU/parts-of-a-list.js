8ca98d70220b29b2e07d96f2fdc5aa7156312189
aaronbush3 <aaronbush3@gmail.com>
2022-11-02 18:49:16 -0400

    return arrays split in different ways

---
+++ 7 KYU/parts-of-a-list.js
@@ -0,0 +1,7 @@
+function partlist(arr){
+    let result = [];
+    for (let i = 1; i < arr.length; i++) {
+        result.push([arr.slice(0, i).join(' '), arr.slice(i).join(' ')]);
+    }
+    return result;
+}
