8cc06c68c0f1b5e512fb736e574c25bd5ba1afe2
aaronbush3 <aaronbush3@gmail.com>
2022-11-02 19:01:10 -0400

    Updated comments

--- 7 KYU/parts-of-a-list.js
+++ 7 KYU/parts-of-a-list.js
@@ -1,7 +1,10 @@
 function partlist(arr){
     let result = [];
-    for (let i = 1; i < arr.length; i++) {
-        result.push([arr.slice(0, i).join(' '), arr.slice(i).join(' ')]);
+    for (let i = 1; i < arr.length; i++) { // loop through the array starting at index 1
+        result.push([arr.slice(0, i).join(' '), arr.slice(i).join(' ')]); 
+        // arr.slice(0, i) returns the first i elements of arr
+        // then we join them with a space
+        // arr.slice(i) returns the rest of the elements of arr
     }
     return result;
 }
