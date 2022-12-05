16c3dc1dcdf8a28b0cc0944c137e870d48f677ae
aaronbush3 <aaronbush3@gmail.com>
2022-12-05 18:55:41 -0500

    Cleaned up code

--- 7 KYU/Filter-unused-digits.js
+++ 7 KYU/Filter-unused-digits.js
@@ -1,15 +1,4 @@
-// Given a varying number of integer arguments, return the digits that are not present in any of them.
-
-//     Example:
-
-// [12, 34, 56, 78]  => "09"
-// [2015, 8, 26]     => "3479"
-
-// Note: the digits in the resulting string should be sorted.
-
-
 function unusedDigits(...args) {
-  let digits = '0123456789';
   let usedDigits = args.join('');
-  return digits.replace(new RegExp(`[${usedDigits}]`, 'g'), '');
+  return '0123456789'.replace(new RegExp(`[${usedDigits}]`, 'g'), '');
 }
