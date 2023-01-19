864a45b5cf5d90b5bf263498d992bb65da4b274a
aaronbush3 <aaronbush3@gmail.com>
2023-01-19 15:52:12 -0500

    Updated code, got carried away and got it wrong before submitting

--- 7 KYU/NiceArray.js
+++ 7 KYU/NiceArray.js
@@ -1,8 +1,7 @@
 function isNice(arr){
+  if (arr.length === 0) return false;
   for (let i = 0; i < arr.length; i++) {
-        if (!(arr.includes(arr[i] - 1) || arr.includes(arr[i] + 1))) {
-            return false;
-        }
-    }
-    return true;
+    if (arr.indexOf(arr[i] + 1) === -1 && arr.indexOf(arr[i] - 1) === -1) return false;
+  }
+  return true;
 }
