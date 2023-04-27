929576db26488b175fc999176484546c0361f8ea
aaronbush3 <aaronbush3@gmail.com>
2023-04-27 10:09:51 -0400

    Count the overall amount of natural numbers that are not divisible by 2, 3, or 5

---
+++ 7 KYU/MysteriousSingularityNumbers.js
@@ -0,0 +1,3 @@
+function countNumbers(n) {
+    return n - (Math.floor(n / 2) + Math.floor(n / 3) + Math.floor(n / 5)) + (Math.floor(n / 6) + Math.floor(n / 10) + Math.floor(n / 15)) - Math.floor(n / 30);
+}
